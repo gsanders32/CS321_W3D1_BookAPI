87295d5a0ce9ea5e09b47265771befbd470465a8
gsanders32 <gray.sanders@gmail.com>
2020-04-14 17:28:56 -0500

    Filled in todos

--- CS321_W3D1_BookAPI/Data/BookContext.cs
+++ CS321_W3D1_BookAPI/Data/BookContext.cs
@@ -6,11 +6,12 @@ namespace CS321_W3D1_BookAPI.Data
     public class BookContext : DbContext
     {
         // TODO: implement a DbSet<Book> property
-
+        public DbSet<Book> Books { get; set; }
         // This method runs once when the DbContext is first used.
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             // TODO: use optionsBuilder to configure a Sqlite db
+            optionsBuilder.UseSqlite("Data Source=Books.db");
         }
 
         // This method runs once when the DbContext is first used.
@@ -21,6 +22,11 @@ namespace CS321_W3D1_BookAPI.Data
             base.OnModelCreating(modelBuilder);
 
             // TODO: configure some seed data in the books table
+            modelBuilder.Entity<Book>().HasData(
+            new Book { Id = 1, Title = "Nineteen Eighty-Four", Author = "George Orwell", Category = "Fiction" },
+            new Book { Id = 2, Title = "Educated", Author = "Tara Westover", Category = "Non-fiction" },
+            new Book { Id = 3, Title = "It", Author = "Stephen King", Category = "Horror" }
+            );
         }
 
     }
